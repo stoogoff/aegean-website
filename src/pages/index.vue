eccbecb31cac0eed23a3a6e581568f979b4f198e
Stoo <stoo.goff@gmail.com>
2025-10-19 16:39:15 +0100

    Updated Isle of Pelops info

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,8 +6,7 @@
 		<section id="pelops" class="buffer">
 			<div>
 				<h2>The Isle of Pelops</h2>
-				<p class="text-xl md:text-3xl"><strong class="aegean">NOW RUNNING!</strong> A new setting guide for the <strong class="aegean">Aegean</strong> role-playing game. <em>The Isle of Pelops</em> will describe the mythology and history of the Peloponnese as well as provide details of important people and locations, including the citadel of Mykenai and Sparta. New options are provided for players and GMs alike, including new backgrounds, Mystery Cults, adversaries, and more!</p>
-				<we-link-action to="https://www.kickstarter.com/projects/stoo-goff/aegean-the-isle-of-pelops" block outlined>Back Now!</we-link-action>
+				<p class="text-xl md:text-3xl"><strong class="aegean">COMING SOON!</strong> A new setting guide for the <strong class="aegean">Aegean</strong> role-playing game. <em>The Isle of Pelops</em> will describe the mythology and history of the Peloponnese as well as provide details of important people and locations, including the citadel of Mykenai and Sparta. New options are provided for players and GMs alike, including new backgrounds, Mystery Cults, adversaries, and more!</p>
 			</div>
 			<cdn-image
 				source="/img/products/isle-of-pelops-cover.jpg"
